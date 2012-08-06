fc22a855a961e7de6bf88151d60e18cdfa9f9046
Rafaell Lins <rafaellmail@gmail.com>
2012-08-06 15:14:00 -0300

    Update dev/extensions/yt-Date.js
    Inclusão de 3 novos métodos para o tipo Date (addDays, subtractDays e toDDMMYYYYString)

--- dev/extensions/yt-Date.js
+++ dev/extensions/yt-Date.js
@@ -4,9 +4,29 @@
 autor: Rafaell Lins
 e-mail: rafaellmail@gmail.com
 criado: 01/10/2011
-última modificação: 24/11/2011
+ï¿½ltima modificaï¿½ï¿½o: 24/11/2011
 *****************************************/
 
 Date.prototype.inInterval = function(date, seconds){
 	return ((date - this) <= (seconds * 1000));
+}
+
+//Retorna nova data com dias somados.
+//The Universal Coordinated Time (UTC)
+//Date.UTC(2012,02,30) -> 1333065600000
+Date.prototype.addDays = function (days) {
+    return new Date(this - (86400000 * days * -1));
+}
+
+//Retorna nova data com dias subtraÃ­dos.
+Date.prototype.subtractDays = function (days) {
+    return new Date(this - (86400000 * days));
+}
+
+Date.prototype.toDDMMYYYYString = function () {
+    return isNaN(this) ? 'NaN' : [
+        this.getDate() > 9 ? this.getDate() : '0' + this.getDate(),
+        this.getMonth() > 8 ? this.getMonth() + 1 : '0' + (this.getMonth() + 1),
+        this.getFullYear()
+    ].join('/') 
 }
