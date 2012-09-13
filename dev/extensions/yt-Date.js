fd4dab06b1fd6333dd14d9efacd9de41f9eb9ad2
Rafaell Lins <rafaellmail@gmail.com>
2012-09-13 15:19:38 -0300

    Update dev/extensions/yt-Date.js
    Correção de comentário...

--- dev/extensions/yt-Date.js
+++ dev/extensions/yt-Date.js
@@ -15,18 +15,18 @@ Date.prototype.inInterval = function(date, seconds){
 //The Universal Coordinated Time (UTC)
 //Date.UTC(2012,02,30) -> 1333065600000
 Date.prototype.addDays = function (days) {
-    return new Date(this - (86400000 * days * -1));
+	return new Date(this - (86400000 * days * -1));
 }
 
 //Retorna nova data com dias subtraídos.
 Date.prototype.subtractDays = function (days) {
-    return new Date(this - (86400000 * days));
+	return new Date(this - (86400000 * days));
 }
 
 /*
 versão beta*
 Possui alguns erros:
-No IE, ao mesclar entre operadores e a sua ausência, como no ex: 
+No IE, ao mesclar entre separadores e a sua ausência, como no ex: 
 (Date.stringFormat("yyyyMM-ddhhmm:ss")) -> O retorno não é como o esperado. 
 Em moteres do mozilla, foram testados e funcionam normalmente.
 */
